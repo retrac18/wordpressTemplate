ffd881ea3ce3029273bc03ec430c2479a5afe6b9
Carter Binarao <carter.binarao@globalzeal.net>
2014-11-02 15:14:32 +0800

    Added necessary folders

---
+++ js/app.js
@@ -0,0 +1,23 @@
+'use strict';
+
+var app = angular.module( 'myApp',
+		[ 'ngRoute' ] );
+
+app.config( [ '$routeProvider',	 function( $routeProvider ) {
+	$routeProvider
+		.when( '/', {
+			templateUrl  : 'partials/tpl/home.tpl.html'
+		})
+		.when( '/Aboutus', {
+			templateUrl  : 'partials/tpl/about.tpl.html'
+		})
+		.when( '/Contactus', {
+			templateUrl  : 'partials/tpl/contact.tpl.html'
+		})
+		.when( '/Portfolio', {
+			templateUrl  : 'partials/tpl/portfolio.tpl.html'
+		})
+		.otherwise( {
+			redirectTo : '/'
+		} );
+} ] );
